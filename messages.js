d1c25154f0cd6b7f0d0ded196929f239fc9ec365
Brent Knudsen <>
2022-05-30 10:37:46 -0600

    added preliminary code for input validation and actions when submit button is clicked

--- messages.js
+++ messages.js
@@ -6,3 +6,13 @@ let commentInput = document.getElementById('commentInput').value;
 
 let profileImage = document.getElementById('profileImagePlaceholder').value;
 
+const submitCommentButton =  document.getElementById('submitButton');
+
+submitCommentButton.addEventListener('click', function () {
+    //code to submit both user name and comment below
+    if (displayNameInput.value == "" || commentInput.value == "") {
+        alert("Please enter display name and/or comment.");
+    } else if (displayNameInput.value == '[^]+' && commentInput.value == '[^]+') {
+        console.log('it worked');
+    }
+})
