d7de4de19cc144d387b854246b4deb30e30c115d
Brent Knudsen <>
2022-05-30 16:18:26 -0600

    added some logic to entry form

--- messages.js
+++ messages.js
@@ -1,18 +1,25 @@
 let comments = [];
 
-let displayNameInput = document.getElementById('displayNameInput').value;
+let displayNameInput = document.getElementById('displayNameInput');
 
-let commentInput = document.getElementById('commentInput').value;
+let commentInput = document.getElementById('commentInput');
 
-let profileImage = document.getElementById('profileImagePlaceholder').value;
+let profileImage = document.getElementById('profileImagePlaceholder');
 
 const submitCommentButton =  document.getElementById('submitButton');
 
 submitCommentButton.addEventListener('click', function () {
-    //code to submit both user name and comment below
-    if (displayNameInput.value == "" || commentInput.value == "") {
-        alert("Please enter display name and/or comment.");
-    } else if (displayNameInput.value == '[^]+' && commentInput.value == '[^]+') {
-        console.log('it worked');
+    if (displayNameInput.value != "" && commentInput.value != "") {
+        comments.push(displayNameInput);
+        let Html = 
+        
+        for (i = 0; i < comments.length - 1; i++) 
+        {
+            
+        }
+    } else if (displayNameInput.value == '' || commentInput.value == '') {
+        displayNameInput.setCustomValidity('Please fill out user name and/or comment');
     }
-})
+    
+} 
+)
