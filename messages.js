099d8442e94e02551bf49c1d82854e6a82f3c183
Brent Knudsen <>
2022-06-20 14:15:50 -0600

    worked on functionality to edit user comment

--- messages.js
+++ messages.js
@@ -19,6 +19,12 @@ let profileImage = document.getElementById('profileImagePlaceholder');
 
 const submitCommentButton =  document.getElementById('submitButton');
 
+function bindEditButtonListeners() {
+    const editCommentButtons = document.querySelectorAll(".inputToEditCommentAndButtonToSubmitEditedComment")
+    Array.from(editCommentButtons).forEach(function(buttonElement) {
+        buttonElement.addEventListener('click', handleEditComment)
+    })
+};
 function renderComments() {
     // everything in comments to be rendered in the DOM in between main tags
     const mainElement = document.getElementsByTagName("main")[0];
@@ -31,38 +37,46 @@ function renderComments() {
     }
    
     mainElement.innerHTML = htmlComment;
+    bindEditButtonListeners();
 }
 
 renderComments();
 
 function handleEditComment(e) {
    
-   const newComment = e.target.parentElement.querySelector("input").value;
+   const commentId = parseInt(e.target.getAttribute("data-commentId"))
+    const newCommentToUse = e.target.parentElement.querySelector("input").value;
    comments = comments.map(function(commentItem) {
+       const isToUpdate = commentId === commentItem.id
+       const previousComment = commentItem.comment;
+       const newComment = isToUpdate ? newCommentToUse : previousComment;
     return {
         ...commentItem,
-        comment: 
+        comment: newComment
     }
    })
 }
 
 function getCommentBoxAsString(displayName, comment, id) {
 
-    return `<div id="profileImagePlaceholderDiv">
-        <i id="profileImagePlaceholder" class="fa-solid fa-user"></i>
+    return `<div class="profileImagePlaceholderDiv">
+        <i class="profileImagePlaceholder" class="fa-solid fa-user"></i>
        </div>
        <div>
-            <h4 id="userName">${displayName}</h4>
-            <div id="editAndDeleteButton">
-                <button id="editButton">Edit</button>
-                <button id="deleteButton">Delete</button>
+            <h4>${displayName}</h4>
+            <div>
+                <button>Edit</button>
+                <button>Delete</button>
             </div>
-            <div id="actualComment">
-                <p id="actualCommentDisplayed">${comment}</p>
+            <div>
+                <p>${comment}</p>
             </div>  
-            <div id="inputToEditCommentAndButtonToSubmitEditedComment">
-                <input type="text" id="inputToEditComment" placeholder="Edit Comment">
-                <button onclick="handleEditComment(${id})" id="submitEditedCommentButton">Submit</button>
+            
+            <div 
+            
+            class="inputToEditCommentAndButtonToSubmitEditedComment">
+                <input type="text" placeholder="Edit Comment">
+                <button data-commentId = "${id}">Submit</button>
             </div>  
             </div>
         </div>`
