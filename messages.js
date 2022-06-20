31a6a1de93fa95e0dbe7c2bf6c977d970fa2814c
Brent Knudsen <>
2022-06-20 13:39:58 -0600

    added functionality for submitting and printing comment to DOM

--- messages.js
+++ messages.js
@@ -1,4 +1,15 @@
-let comments = [];
+let comments = [
+    {
+        displayName: 'Brent',
+        comment: 'a comment',
+        id: 0
+    },
+    {
+        displayName: "Charlie",
+        comment: 'another comet',
+        id: 1
+    }
+];
 
 let displayNameInput = document.getElementById('displayNameInput');
 
@@ -8,16 +19,69 @@ let profileImage = document.getElementById('profileImagePlaceholder');
 
 const submitCommentButton =  document.getElementById('submitButton');
 
+function renderComments() {
+    // everything in comments to be rendered in the DOM in between main tags
+    const mainElement = document.getElementsByTagName("main")[0];
+    
+    let htmlComment = '';
+    const commentsOrdered = [...comments].reverse();
+    for (let i = 0; i < commentsOrdered.length; i++) {
+        const { displayName, comment } = commentsOrdered[i];
+        htmlComment += getCommentBoxAsString(displayName, comment, i);
+    }
+   
+    mainElement.innerHTML = htmlComment;
+}
+
+renderComments();
+
+function handleEditComment(e) {
+   
+   const newComment = e.target.parentElement.querySelector("input").value;
+   comments = comments.map(function(commentItem) {
+    return {
+        ...commentItem,
+        comment: 
+    }
+   })
+}
+
+function getCommentBoxAsString(displayName, comment, id) {
+
+    return `<div id="profileImagePlaceholderDiv">
+        <i id="profileImagePlaceholder" class="fa-solid fa-user"></i>
+       </div>
+       <div>
+            <h4 id="userName">${displayName}</h4>
+            <div id="editAndDeleteButton">
+                <button id="editButton">Edit</button>
+                <button id="deleteButton">Delete</button>
+            </div>
+            <div id="actualComment">
+                <p id="actualCommentDisplayed">${comment}</p>
+            </div>  
+            <div id="inputToEditCommentAndButtonToSubmitEditedComment">
+                <input type="text" id="inputToEditComment" placeholder="Edit Comment">
+                <button onclick="handleEditComment(${id})" id="submitEditedCommentButton">Submit</button>
+            </div>  
+            </div>
+        </div>`
+
+}
+
 submitCommentButton.addEventListener('click', function () {
-    if (displayNameInput.value != "" && commentInput.value != "") {
-        comments.push(displayNameInput);
-        let Html = 
-        
-        for (i = 0; i < comments.length - 1; i++) 
-        {
-            
-        }
-    } else if (displayNameInput.value == '' || commentInput.value == '') {
+    const newDisplayName = displayNameInput.value;
+    const newDisplayComment = commentInput.value;
+    const newComment = {
+        displayName: newDisplayName,
+        comment: newDisplayComment,
+        id: comments.length
+    }
+    const isFilledOutCorrectly = newDisplayName != "" && newDisplayComment != '';
+    if (isFilledOutCorrectly) {
+        comments.push(newComment);
+        renderComments();
+    } else {
         displayNameInput.setCustomValidity('Please fill out user name and/or comment');
     }
     
