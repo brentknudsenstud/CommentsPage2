9b05d430aa4a08e8a97da69e4c58d3d6a1850155
Brent Knudsen <brent.knudsen8710@stu.mtec.edu>
2023-01-03 21:41:32 -0700

    added pseudo code inside export function editComment to outline steps of how to write the code for retaining original comment to input box when edit button is clicked

--- src/state.js
+++ src/state.js
@@ -22,5 +22,9 @@ export function deleteComment(idToDelete) {
 export function editComment({ id, comment }) {
     const commentToUpdate = comments.find(({ id: id1 }) => id === id1);
     // add functionality so that original comment to edit shows up in input box
+    // psuedo code below
+    // if edit button is clicked,
+    // then grab current comment and attach current comment and display in edit input box
+    // then when sumbit button is clicked, replace current comment with edited version of comment.
     commentToUpdate.comment = comment;
 }
