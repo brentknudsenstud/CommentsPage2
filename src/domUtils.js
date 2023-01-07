29e5584e95a60c345834049455edd370cfe64f5c
Brent Knudsen <brent.knudsen8710@stu.mtec.edu>
2023-01-06 23:13:26 -0700

    added comment value in edit input box so the orginal comment shows up in input box when edit button is selected

--- src/domUtils.js
+++ src/domUtils.js
@@ -88,7 +88,7 @@ function getCommentBoxAsString(name, comment, id, photo) {
                 
                     <div 
                     class="inputToEditCommentAndButtonToSubmitEditedComment hide">
-                        <input data-editCommentInput="${id}" type="text" placeholder="Edit Comment" id='newInputBox' class="input-color-change">
+                        <input data-editCommentInput="${id}" type="text" placeholder="Edit Comment" id='newInputBox' value="${comment}" class="input-color-change">
                         <button data-submitButtonId="${id}" id="submitEditedCommentButton" style="text-shadow: 1px 1px 4px gray; color: green; font-size: 28px">&#10146;</button>
                     </div>  
                 </div>
